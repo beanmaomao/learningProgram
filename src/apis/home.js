e15528ccda8a8c0654c9a58679f1f050c72b613b
beanmaomao <asisterdoog@Gmail.com>
2024-04-01 11:26:56 +0800

    home-面板组件封装+home-新鲜好物业务实现

--- src/apis/home.js
+++ src/apis/home.js
@@ -1,7 +1,22 @@
 import httpInstance from '@/utils/http'
+
 //获取banner
 export function getBannerAPI(){
     return httpInstance({
         url:'/home/banner'
+    })
+}
+
+
+/**
+ * @description: 获取新鲜好物
+ * @param {*}
+ * @return {*}
+ */
+
+//获取new
+export const findNewAPI = ()=>{
+    return httpInstance({
+        url:'/home/new'
     })
 }
