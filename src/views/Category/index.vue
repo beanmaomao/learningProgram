b957c3f500312182a394b16eba3d7b24cba54fbc
beanmaomao <asisterdoog@Gmail.com>
2024-04-07 20:13:55 +0800

    解决路由缓存问题

--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -2,14 +2,23 @@
 import { getCategoryAPI } from '@/apis/category'
 import { ref,onMounted } from 'vue'
 import GoodsItem from '../Home/components/GoodsItem.vue';
+import { onBeforeRouteUpdate } from 'vue-router';
+
+//路由参数变化时，把分类数据接口重新发送
+onBeforeRouteUpdate((to)=>{
+  //使用最新的路由参数请求最新的分类数据：route.params.id属于滞后数据
+  //"to"能获取目标路径参数
+  getCategory(to.params.id)
+})
+
 //引入函数在组件内部获取路由参数
 import { useRoute } from 'vue-router'
-
 //请求二级分类参数(渲染面包屑)
 const categoryData=ref({})
 const route=useRoute()
-const getCategory= async () =>{
-  const res=await getCategoryAPI(route.params.id)
+//默认是route的id参数，当传入参数时会变成新的id参数
+const getCategory= async (id=route.params.id) =>{
+  const res=await getCategoryAPI(id)
   categoryData.value=res.result
 }
 onMounted(()=>getCategory())
@@ -26,6 +35,7 @@ const getBanner=async()=>{
     bannerList.value=res.result
 }
 onMounted(()=>getBanner())
+
 </script>
 
 <template>
