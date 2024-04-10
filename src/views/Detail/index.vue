9d80f629b82adb31b5bfae8c03d0ad709769ee85
beanmaomao <asisterdoog@Gmail.com>
2024-04-10 20:30:02 +0800

    详情热榜

--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -2,6 +2,7 @@
 import { getDetail  } from '@/apis/detail';
 import { onMounted, ref } from'vue'
 import { useRoute } from 'vue-router';
+import DetailHot from './components/DetailHot.vue'
 
 const route=useRoute()
 const goods=ref({})
@@ -56,7 +57,7 @@ onMounted(()=>getGoods())
                 </li>
                 <li>
                   <p>品牌信息</p>
-                  <p>{{ goods.brand.name }}</p>
+                  <p></p>
                   <p><i class="iconfont icon-dynamic-filling"></i>品牌主页</p>
                 </li>
               </ul>
@@ -107,19 +108,23 @@ onMounted(()=>getGoods())
                 <div class="goods-detail">
                   <!-- 属性 -->
                   <ul class="attrs">
-                    <li v-for="item in goods.details.properties" :key="item.value">
+                    <!-- <li v-for="item in goods.details.properties" :key="item.value">
                       <span class="dt">{{ item.name }}</span>
                       <span class="dd">{{ item.value }}</span>
-                    </li>
+                    </li> -->
                   </ul>
                   <!-- 图片 -->
-                  <img :key="img" :src="img" alt="" v-for="img in goods.details.pictures">
+                  <!-- <img :key="img" :src="img" alt="" v-for="img in goods.details.pictures"> -->
                 </div>
               </div>
             </div>
             <!-- 24热榜+专题推荐 -->
             <div class="goods-aside">
-
+              <!-- 加冒号识别传入数据类型 -->
+              <!-- 24小时热榜 -->
+              <DetailHot :hot-type="1" />
+              <!-- 周热榜 -->
+              <DetailHot :hot-type="2" />
             </div>
           </div>
         </div>
