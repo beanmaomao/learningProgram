f5c693490ae92c75240f48a6467e6ad604304ff2
beanmaomao <asisterdoog@Gmail.com>
2024-04-13 21:10:17 +0800

    列表购物车基础渲染

--- src/router/index.js
+++ src/router/index.js
@@ -8,7 +8,7 @@ import Home from '@/views/Home/index.vue'
 import Category from '@/views/Category/index.vue'
 import SubCategory from '@/views/SubCategory/index.vue'
 import Detail from '@/views/Detail/index.vue'
-
+import CarList from '@/views/CartList'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   //配置path和component规则
@@ -33,6 +33,10 @@ const router = createRouter({
         {
           path:'detail/:id',
           component:Detail
+        },
+        {
+          path:'cartlist',
+          component:CarList
         }
       ]
     },
--- src/views/Layout/components/HeaderCart.vue
+++ src/views/Layout/components/HeaderCart.vue
@@ -33,7 +33,7 @@ const cartStore=useCartStore()
           <!-- .toFixed[2]保留两位小数 -->
           <p> &yen;{{ cartStore.allPrice.toFixed[2]}}</p>
         </div>
-        <el-button size="large" type="primary" >去购物车结算</el-button>
+        <el-button size="large" type="primary" @click="$router.push('/cartList')">去购物车结算</el-button>
       </div>
     </div>
 </div>
